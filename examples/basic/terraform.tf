de0e410319e2249bf294fe5a9bb422c389ec276a
Rohith Jayawardene <gambol99@gmail.com>
2025-10-02 16:48:09 +0100

    chore: updated the version of terraform

--- examples/basic/terraform.tf
+++ examples/basic/terraform.tf
@@ -1,11 +1,11 @@
 
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.10.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.34"
+      version = ">= 6.0.0"
     }
   }
 }
