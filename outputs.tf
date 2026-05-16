1caf61806925ab4a5237fe877f42d97802c69f46
Rohith Jayawardene <gambol99@gmail.com>
2026-05-16 10:41:24 +0100

    feat: added the additional outputs from the module

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,6 @@
-output "cluster_endpoint" {
-  description = "The endpoint for the EKS Kubernetes API"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_certificate_authority_data" {
-  description = "The base64 encoded certificate data for the EKS cluster"
-  value       = module.eks.cluster_certificate_authority_data
-}
-
-output "cluster_name" {
-  description = "The name of the EKS cluster."
-  value       = module.eks.cluster_name
+output "account_id" {
+  description = "The AWS account ID."
+  value       = local.account_id
 }
 
 output "cluster_arn" {
@@ -18,24 +8,44 @@ output "cluster_arn" {
   value       = module.eks.cluster_arn
 }
 
+output "cluster_certificate_authority_data" {
+  description = "The base64 encoded certificate data for the EKS cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint for the EKS Kubernetes API"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  description = "The name of the EKS cluster."
+  value       = module.eks.cluster_name
+}
+
 output "cluster_oidc_provider_arn" {
   description = "The ARN of the OIDC provider for the EKS cluster"
   value       = module.eks.oidc_provider_arn
 }
 
-output "account_id" {
-  description = "The AWS account ID."
-  value       = local.account_id
+output "cluster_primary_security_group_id" {
+  description = "The ID of the primary security group for the EKS cluster"
+  value       = module.eks.cluster_primary_security_group_id
+}
+
+output "cluster_security_group_id" {
+  description = "The ID of the security group for the EKS cluster"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "cluster_version" {
+  description = "The Kubernetes version of the EKS cluster"
+  value       = module.eks.cluster_version
 }
 
 output "cross_account_role_arn" {
   description = "The cross account arn when we are using a hub"
   value       = local.enable_cross_account_role ? try(aws_iam_role.argocd_cross_account_role[0].arn, null) : null
-}
-
-output "region" {
-  description = "The AWS region in which the cluster is provisioned"
-  value       = local.region
 }
 
 output "ebs_csi_driver_pod_identity_arn" {
@@ -47,3 +57,24 @@ output "efs_csi_driver_pod_identity_arn" {
   description = "The ARN of the EFS CSI driver pod identity"
   value       = local.enable_efs_csi_driver ? module.aws_efs_csi_pod_identity[0].iam_role_arn : null
 }
+
+output "kms_key_arn" {
+  description = "The ARN of the KMS key used for encrypting secrets in the EKS cluster"
+  value       = module.eks.kms_key_arn
+}
+
+output "kms_key_id" {
+  description = "The ID of the KMS key used for encrypting secrets in the EKS cluster"
+  value       = module.eks.kms_key_id
+}
+
+output "node_security_group_id" {
+  description = "The ID of the security group for the EKS cluster nodes"
+  value       = module.eks.node_security_group_id
+}
+
+output "region" {
+  description = "The AWS region in which the cluster is provisioned"
+  value       = local.region
+}
+
